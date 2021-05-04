51940134e36dbb6781c30275b29ba65ca08be09c
Felicitas <felifigueroaf@gmail.com>
2021-05-04 20:48:48 -0300

    Added Login

--- src/App.js
+++ src/App.js
@@ -1,58 +1,46 @@
-import React, { useState } from 'react';
-import SearchBar from './components/SearchBar';
-import Heros from './components/Heros';
-import Header from './components/Header';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from './Login'
-//My token: 1046974402498670
+import React , {useState} from 'react';
+import '../App2.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Heros from './Heros.js';
+import SearchBar from './SearchBar.js';
+import Header from './Header.js'
 
 
-function App() {
-  const [searchText, setSearchText] = useState('');
-  const [superheroData, setSuperheroData] = useState([]);
-  
+const Home = () => {
+    
+    const [searchText, setSearchText] = useState('');
+    const [superheroData, setSuperheroData] = useState([]);
+
   // fx search
-  
-  async function searchSuperHeroes () {
+
+    async function searchSuperHeroes () {
     const response = await fetch(`https://www.superheroapi.com/api.php/1046974402498670/search/${searchText}`);
     const data = await response.json();
     console.log("searchSuperHeroes -> data", data)
     setSuperheroData(data.results);    
-  }
+    }
 
-  function handleChange (e) {
+    function handleChange (e) {
     const searchTerm = e.target.value;
     
     setSearchText(searchTerm);
     if (searchTerm.length === 0) {
-      setSuperheroData([]);
+        setSuperheroData([]);
     }
     if (searchTerm.length > 3) {
-      searchSuperHeroes();
+        searchSuperHeroes();
     }
-  }
+    }
+    
 
-  return (
-    <div className="container">
-      <div className="main">
+    return (
+        <React.Fragment>
         <Header /> 
         <SearchBar searchText={searchText} handleChange={handleChange} />
-        <Heros superheroData={superheroData} />
-        {/* <Router>
-        <Switch>
-          <Route exact path='/' component={Login}/>
-        </Switch>
-        </Router> */}
-      </div>
-    </div>
-  );
-  }
+        <Heros superheroData={superheroData} /> 
+        </React.Fragment>
+        
+    )
+}
 
-
-export default App;
+export default Home
