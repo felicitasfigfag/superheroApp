109d0f145d116c08f102ee6d7aa95d89e9e43a89
Felicitas <felifigueroaf@gmail.com>
2021-05-04 20:53:29 -0300

    Update HeroIndividual.js

--- src/components/HeroIndividual.js
+++ src/components/HeroIndividual.js
@@ -1,31 +1,26 @@
-import React, {useState} from 'react';
+import React , {useState} from 'react';
 
 //Aca manejamos individualmente a cada heroe
 //primero como resultado de la busqueda
 
 function HeroIndividual(props) {
   const {data = []} = props;
-  //state de selectHero(heroindividual)
+
   const [selectedHero, setSelectedHero] = useState([]);
 
-function getDivs(){
-        let divsOnDisplay = [document.querySelectorAll('.card')]
-        console.log(divsOnDisplay)
-      }
-      
-  async function getHero (heroData) {
-      console.log('you selected hero : ', heroData)
-      getDivs()
-    }
+  function getHero (heroData) {
+    console.log('you selected hero : ', heroData)
+    setSelectedHero([...selectedHero, heroData])
+    console.log(selectedHero)
+  }
 
-      
-  
   return (
-    <div className="card">
+    <div className="card" >
       <div className="add">
           <button 
           className="add-btn" 
-          onClick={() => {getHero(data)}}> 
+          id={data.id}
+          onClick={ () => getHero(data)}>
           + </button>
       </div>
       <div className="card-inner">
