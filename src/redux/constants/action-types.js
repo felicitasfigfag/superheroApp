fcc1d61947d06c16f2dbcea14b8408169356a759
Felicitas <felifigueroaf@gmail.com>
2021-06-02 22:51:05 -0300

    Add files via upload

---
+++ src/redux/constants/action-types.js
@@ -0,0 +1,8 @@
+//This will contain all the action types
+export const ActionTypes = {
+    SET_HEROS: 'SET_HEROS',
+    SELECTED_HERO: 'SELECTED_HERO',
+    REMOVE_SELECTED_HERO: 'REMOVE_SELECTED_HERO',
+    ADD_TEAM_HERO: 'ADD_TEAM_HERO',
+    REMOVE_TEAM_HERO: 'REMOVE_TEAM_HERO'
+};
