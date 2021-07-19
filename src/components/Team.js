d962f5b6cb4c302586de62408b1c4d85e34a0331
Felicitas <felifigueroaf@gmail.com>
2021-07-19 12:02:15 -0300

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,58 +1,86 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import TeamHero from './TeamHero';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector, connect} from 'react-redux';
+import {removeTeamHero} from '../redux/actions/heroActions';
 
-const Team = () => {
+
+const Team = ({removeTeamHero}) => {
 const globalStats = {strength: 0, intelligence: 0, speed: 0, durability: 0, power: 0, combat: 0 }
-//let {strength, intelligence, speed, durability, power, combat} = globalStats
-const teamHero = useSelector((state) => state.teamHero);
+const team = useSelector((state) => state.team);
+
 
 //STATS
 const stats = () =>{
-    if(teamHero.length > 0){
-        for(let i = 0; i < teamHero.length; i++){
-            const powstat = teamHero[i].powerstats;
-            globalStats.strength +=  parseInt(powstat.strength);
-            globalStats.intelligence +=  parseInt(powstat.intelligence);
-            globalStats.speed +=  parseInt(powstat.speed);
-            globalStats.durability += parseInt(powstat.durability);
-            globalStats.power += + parseInt(powstat.power);
-            globalStats.combat +=  + parseInt(powstat.combat);}
-
-        }else{
-        console.log("no info")
-    }
-    
-}
+    if(team.length > 0){
+        for(let i = 0; i < team.length; i++){
+            const powstat = team[i].powerstats;
+            for(let stat in powstat){
+                if(powstat[stat] !== "null"){
+                    globalStats[stat] += parseInt(powstat[stat])
+                }
+                else{
+                    globalStats[stat] += 0
+                }
+            }
+        }
+}}
 stats();
+
+
+//Team Type
 let arr = [];
-let maxValue = 0;
-for (let prop in globalStats){
-    arr.push(globalStats[prop]);
-    maxValue = Math.max.apply(Math.max, arr);
-    console.log(maxValue)
-}
-// obtuve el maximo valor pero no pude reunir el valor con la key de objeto luego
-//intente iterar de nuevo y comparar con el valor maximo pero no funciono.
-for (let prop in globalStats){
-    if( prop === maxValue){
-        console.log("max value is ", globalStats[prop])
+let teamType = ""
+const teamDetail = () => {
+    if(team.length > 0){
+        //defining type of team
+        for (let stat in globalStats){
+            arr.push(globalStats[stat]);
+        }
+        const maxValue = Math.max(...arr)
+        for (let stat in globalStats){
+            if(globalStats[stat] === maxValue){
+                teamType = stat + " team";
+            }
+        }
     }
-}
+    //if the team is empty
+else{
+    teamType= ""
+}}
+teamDetail()
+
+// Restrictions
+const teamCount = { 
+    good : 0,
+    bad: 0
+};
+const restrictions = () =>{
+    if(team.length > 0){
+        for(let i = 0; i < team.length; i++){ 
+            let alig = team[i].biography.alignment
+            
+            if(teamCount[alig] < 3){
+                teamCount[alig] ++;
+            }
+            else if(teamCount[alig] >= 3){
+            alert("You can only have 3 heros per alignment (Good/Bad) Delete a hero to add another one")
+            removeTeamHero(team[(team.length -1)])
+            }
+        }
+    }}
+restrictions();
 
     return (<>
         <div className="team-container">
-        <Link to={`/`}>
-            <button className="dt-btn">Back</button>
-        </Link>
-            <section className="team-left">
+            <h1 className="dt-name">my team</h1>
+
+            <section className="team-card-grid">
                 <TeamHero />
             </section>
-        <section className="team-rigth">
-            <h1 className="dt-name">My team</h1>
+
+        <section className="team-stats">
                 <ul className="dt-ul">
-                    <li><h2 className="dt-name">Team stats</h2></li>
+                    <li><h3 className="dt-name">Team stats</h3></li>
                     <li className="dt-stat"><b>Strength : </b> {globalStats.strength}</li>
                     <li className="dt-stat"><b>Intelligence : </b>{globalStats.intelligence}</li>
                     <li className="dt-stat"> <b>Speed : </b>{globalStats.speed}</li>
@@ -60,10 +88,30 @@ for (let prop in globalStats){
                     <li className="dt-stat"> <b>Power : </b>{globalStats.power}</li>
                     <li className="dt-stat"> <b>Combat : </b>{globalStats.combat}</li>
                 </ul>
+                <div className="counter">
+                    <span className="count-info">
+                        <p><b>You can have up to six Team members</b></p>
+                        
+                    </span>
+                    <span className= "good-counter counters">
+                        <p className="counter-desc">Good</p>
+                        <p className="counter-value">{teamCount.good}/3</p>
+                    </span>
+                    <span className= "bad-counter counters">
+                        <p className="counter-desc">Bad</p>
+                        <p className="counter-value">{teamCount.bad}/3</p>
+                    </span>
+                </div>
+                <h5 className="team-type">{teamType}</h5>
         </section>
         </div> 
         </>      
             )
-                    }
-
-export default Team
+        }
+        const mapDispatchToProps = dispatch => {
+            return {
+                removeTeamHero: (id) => dispatch(removeTeamHero(id)),
+                
+            }}
+    
+export default connect (null, mapDispatchToProps)(Team)
