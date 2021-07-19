459d9add5af02abac2cbe4be7d31f24a001d675b
Felicitas <felifigueroaf@gmail.com>
2021-07-19 12:07:12 -0300

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,17 +4,18 @@ function SearchBar({handleChange, searchText}) {
 //props from heroListing
   return (
     <section className="search">
-    <form>
-      <input
-        id="search-bar"
-        type="text"
-        className="form-control"
-        placeholder="Search Superheros"
-        autoFocus
-        onChange={handleChange}
-        value={searchText}
-      />
-    </form>
+      <form>
+        <input
+          id="search-bar"
+          type="text"
+          className="form-control"
+          placeholder="Search Superheros"
+          autoFocus
+          onChange={handleChange}
+          value={searchText}
+        />
+      </form>
+      
     </section >
   );
 }
