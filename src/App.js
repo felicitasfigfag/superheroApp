4da7945d67a3e5a8827ac909664a9a7248e97d2e
Felicitas <felifigueroaf@gmail.com>
2021-07-19 12:12:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HeroListing from './components/HeroListing';
 import HeroDetail from './components/HeroDetail';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Team from './components/Team'
-import Login from './components/Login'
+// import Login from './components/Login'
 //My token: 1046974402498670
 
 function App() {
@@ -14,10 +14,10 @@ function App() {
           <Router>
             <Header />
             <Switch>
-              <Route path="/" exact component ={Login}/>
-              <Route path="/home" exact component={HeroListing}/>
-              <Route path="/hero/:heroId" exact component={HeroDetail}/>
-              <Route path="/team" exact component={Team}/>
+              {/* <Route exact path="/"  component ={Login}/> */}
+              <Route exact path="/" component={HeroListing}/>
+              <Route exact path="/hero/:heroId" component={HeroDetail}/>
+              <Route exact path="/team" component={Team}/>
               <Route>404 not found!</Route>
             </Switch>
           </Router>
