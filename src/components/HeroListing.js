49a95b5f7265e42b50685cad0e0a0fa38ca28359
Felicitas <felifigueroaf@gmail.com>
2021-07-19 12:06:25 -0300

    Update HeroListing.js

--- src/components/HeroListing.js
+++ src/components/HeroListing.js
@@ -4,13 +4,23 @@ import HeroComponent from "./HeroComponent";
 import SearchBar from './SearchBar';
 import {setHeros} from '../redux/actions/heroActions';
 import axios from 'axios';
-import {Link} from "react-router-dom";
-
 
 const HeroListing = () => {
-    //const heros = useSelector((state) => state);
     const [searchText, setSearchText] = useState('');
     const dispatch = useDispatch();
+
+
+         //esta maneja el cambio en el input y dispara searchSuperHeros cuando 
+        //recibe mas de 2 letras
+        function handleChange (e) {
+            const searchTerm = e.target.value;
+            setSearchText(searchTerm);
+            if (searchTerm.length === 0) {
+                setSearchText([]);
+            }
+            else if (searchTerm.length > 2) {
+                searchSuperHeroes();
+        }}
 
     //esta obtiene el valor de input, busca en la api y lo despacha a la store
     const searchSuperHeroes = async () => {
@@ -22,23 +32,9 @@ const HeroListing = () => {
                 
             dispatch(setHeros(response.data));
         };
-        //esta maneja el cambio en el input y dispara la de arriba cuando 
-        //recibe mas de 2 letras
-    function handleChange (e) {
-        const searchTerm = e.target.value;
-        setSearchText(searchTerm);
-        if (searchTerm.length === 0) {
-            setSearchText([]);
-        }
-        if (searchTerm.length > 2) {
-            searchSuperHeroes();
-    }
-        }
+
         return (<div> 
             <SearchBar handleChange = {handleChange} searchText = {searchText}/>
-            <Link to={'/team'}>
-            <button className="dt-btn nav-btn">My team</button>
-            </Link>
                     <div className= "card-container">
                         <HeroComponent/> 
                     </div>
